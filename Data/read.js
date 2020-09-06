3adc3beb1727767d6ba549a440975e243317b3d8
RNandgave <vedh.n513@gmail.com>
2020-09-06 17:01:55 +0530

    changes to display of data on upper right corner

--- Data/read.js
+++ Data/read.js
@@ -1,12 +1,21 @@
-var data = null
+var data = null;
+const grid = document.querySelector(".rightup");
 
 function read() {
-  data = document.querySelector(".array").value
-  data = data.split(",")
-  for(let i =0; i<data.length; i++)
-  data[i] = parseInt(data[i]);
+  let squares = [];
+  data = document.querySelector(".array").value;
+  data = data.split(",");
+  size = data.length;
+  for (let i = 0; i < size; i++) {
+    const square = document.createElement("div");
+    square.setAttribute("id", "gr" + i);
+    data[i] = parseInt(data[i]);
+    square.append(data[i]);
+    grid.appendChild(square);
+    squares.push(square);
+  }
 }
 
-function display(){
+function display() {
   console.log(data);
 }
